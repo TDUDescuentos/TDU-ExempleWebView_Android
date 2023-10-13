efd9ea306377a7351025dd41e7ebb3cf923608b9
George <jorge.toribio@tdu.com.mx>
2023-10-13 15:35:47 -0600

    Update MainActivity

---
+++ app/src/main/java/com/example/tduwebview_java/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.tduwebview_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings=mywebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webSettings.setGeolocationEnabled(true);
+
+        mywebView.setWebViewClient(new mywebClient(){
+            public void onGeolocationPermissionsShowPrompt(
+                    String origin,
+                    GeolocationPermissions.Callback callback){
+                callback.invoke(origin,true,false);
+            }
+
+
+        });
+
+        // URL DE VISTA
+        mywebView.loadUrl("https://tdu.com.mx");
+
+        mywebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("myApplication", consoleMessage.message() + " -- From line " +
+                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
+            }
+
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+
+
+        });
+    }
+
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon ){
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+}
